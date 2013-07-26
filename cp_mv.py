370297eeb84b1c234bb1bd20c6db9a532ba7eeab
wesleybowman <wesley.bowman23@gmail.com>
2013-07-26 09:10:57 -0300

    copy and move/remove files

---
+++ cp_mv.py
@@ -0,0 +1,26 @@
+''' Right now the sourcefile and destination could also be paths, but the
+    variables need to be passed as strings. This could easily be changed for
+    convenience. '''
+
+import shutil
+import os
+
+def copySingleFile(sourcefile,destination):
+
+    shutil.copy(sourcefile,destination)
+
+def copyDirectory(sourcefile,destination):
+
+    shutil.copytree(sourcefile,destination)
+
+def move(sourcefile,destination):
+
+    shutil.move(sourcefile,destination)
+
+def removeSingleFile(sourcefile):
+
+    os.remove(sourcefile)
+
+def removeDirectory(sourcefile):
+
+    shutil.rmtree(sourcefile)
